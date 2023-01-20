7acebd8e3151cd53f7a45e32c5940942d1560f3c
Konstantin <don.kamatoz@bk.ru>
2023-01-20 12:58:10 +0500

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Константин Стольников on 2023/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
